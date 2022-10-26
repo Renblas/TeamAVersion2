7a0dea95cdbdb0ac41ead6f143b6698ab3abe315
Renblas <renblas.dev@gmail.com>
2022-10-26 16:03:10 -0400

    hi

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,44 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+
+motor frontLeftMotor = motor(PORT1, ratio18_1, false);
+motor backLeftMotor = motor(PORT2, ratio18_1, false);
+motor frontRightMotor = motor(PORT9, ratio18_1, true);
+motor backRightMotor = motor(PORT8, ratio18_1, true);
+
+motor_group leftMotorGroup = motor_group(frontLeftMotor, backLeftMotor);
+motor_group rightMotorGroup = motor_group(frontRightMotor, backRightMotor);
+
+
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 
 using namespace vex;
 
+// Main Robot Class
+class robot {
+  public:
+    // variables
+    int leftDrive;
+    int rightDrive;
+    bool totalDrive_Bool;
+
+    
+
+    // functions   
+    robot();
+    void updateMotors();
+    void updateScreen();
+    void getUserInput();
+    int processAxis(int input, int cutoff);
+};
+
 // A global instance of competition
 competition Competition;
-
-// define your global instances of motors and other devices here
+brain Brain;
+controller Controller;
 
 
 void pre_auton(void) {
@@ -40,9 +68,9 @@ void usercontrol(void) {
   }
 }
 
-//
-// Main will set up the competition functions and callbacks.
-//
+/*
+* Main will set up the competition functions and callbacks.
+*/
 int main() {
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
@@ -55,4 +83,37 @@ int main() {
   while (true) {
     wait(100, msec);
   }
+}
+
+
+/*
+*   Robot Class Definitions
+*/
+
+// Constructor definition outside the class
+robot::robot() {
+  
+}
+
+void robot::updateMotors() {
+    leftMotorGroup.setVelocity(leftDrive, percent);
+    rightMotorGroup.setVelocity(rightDrive, percent);
+
+    leftMotorGroup.spin(fwd);
+    rightMotorGroup.spin(fwd);
+}
+
+void robot::updateScreen() {
+
+}
+
+void robot::getUserInput() {
+    leftDrive = processAxis(Controller.Axis1.value(), 5);
+    rightDrive = processAxis(Controller.Axis1.value(), 5);
+}
+
+int robot::processAxis(int input, int cutoff) {
+    float result = input;
+    if (result <= cutoff && result >= -cutoff) result = 0;
+    return result;
 }
