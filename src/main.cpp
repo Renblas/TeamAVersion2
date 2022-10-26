a00f59f317f861b4cc4bd8098f791c9d362bc9a4
Renblas <renblas.dev@gmail.com>
2022-10-26 18:10:21 -0400

    h

--- src/main.cpp
+++ src/main.cpp
@@ -21,37 +21,34 @@ motor backRightMotor = motor(PORT8, ratio18_1, false);
 motor_group leftMotorGroup = motor_group(frontLeftMotor, backLeftMotor);
 motor_group rightMotorGroup = motor_group(frontRightMotor, backRightMotor);
 
-
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 // Main Robot Class
-class robot {
-  public:
-    // variables
-    int leftDrive;
-    int rightDrive;
-    bool totalDrive_Bool;
+class robot
+{
+public:
+  // variables
+  int leftDrive;
+  int rightDrive;
+  bool totalDrive_Bool;
 
-    
-
-    // functions   
-    robot();
-    void updateMotors();
-    void updateScreen();
-    void getUserInput();
-    int processAxis(int input, int cutoff);
+  // functions
+  robot();
+  void updateMotors();
+  void updateScreen();
+  void getUserInput();
+  int processAxis(int input, int cutoff);
 };
 
 // custom button class
-class customButton {
-    public:
-        // variables
-        bool isToggle;
+class customButton
+{
+public:
+  // variables
+  bool isToggle;
 
-        // function
-        customButton(bool toggle);
-
-
+  // function
+  customButton(bool toggle);
 };
 
 // A global instance of competition
@@ -59,25 +56,24 @@ competition Competition;
 controller Controller;
 robot Robot;
 
-void pre_auton(void) {
+void pre_auton(void)
+{
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 }
 
-
-void autonomous(void) {
-  
+void autonomous(void)
+{
 }
 
-
-void usercontrol(void) {
+void usercontrol(void)
+{
   // User control code here, inside the loop
-  while (1) {
-    
+  while (1)
+  {
+
     Robot.getUserInput();
     Robot.updateMotors();
-
-
 
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
@@ -85,9 +81,10 @@ void usercontrol(void) {
 }
 
 /*
-* Main will set up the competition functions and callbacks.
-*/
-int main() {
+ * Main will set up the competition functions and callbacks.
+ */
+int main()
+{
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
@@ -96,48 +93,52 @@ int main() {
   pre_auton();
 
   // Prevent main from exiting with an infinite loop.
-  while (true) {
+  while (true)
+  {
     wait(100, msec);
   }
 }
 
-
 /*
-*   Robot Class Definitions
-*/
+ *   Robot Class Definitions
+ */
 
 // Constructor definition outside the class
-robot::robot() {
-  
+robot::robot()
+{
 }
 
-void robot::updateMotors() {
-    leftMotorGroup.setVelocity(leftDrive, percent);
-    rightMotorGroup.setVelocity(rightDrive, percent);
+void robot::updateMotors()
+{
+  leftMotorGroup.setVelocity(leftDrive, percent);
+  rightMotorGroup.setVelocity(rightDrive, percent);
 
-    leftMotorGroup.spin(fwd);
-    rightMotorGroup.spin(fwd);
+  leftMotorGroup.spin(fwd);
+  rightMotorGroup.spin(fwd);
 }
 
-void robot::updateScreen() {
-
+void robot::updateScreen()
+{
 }
 
-void robot::getUserInput() {
-    leftDrive = processAxis(Controller.Axis3.value(), 5);
-    rightDrive = processAxis(Controller.Axis2.value(), 5);
+void robot::getUserInput()
+{
+  leftDrive = processAxis(Controller.Axis3.value(), 5);
+  rightDrive = processAxis(Controller.Axis2.value(), 5);
 }
 
-int robot::processAxis(int input, int cutoff) {
-    float result = input;
-    if (result <= cutoff && result >= -cutoff) result = 0;
-    return result;
+int robot::processAxis(int input, int cutoff)
+{
+  float result = input;
+  if (result <= cutoff && result >= -cutoff)
+    result = 0;
+  return result;
 }
 
 /*
-*   Button class definitions
-*/
+ *   Button class definitions
+ */
 
-customButton::customButton() {
-    s
+customButton::customButton(bool toggle)
+{
 }
